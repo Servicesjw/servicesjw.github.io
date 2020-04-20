b5a9319824967e258d8443ca02abc8adfe877f40
Servicesjw <64030279+Servicesjw@users.noreply.github.com>
2020-04-20 19:28:41 +0300

    Add files via upload

---
+++ js/addSchedule.js
@@ -0,0 +1,95 @@
+months = ['Error','січня',"лютого","березня","квітня","травня","червня","липня","серпня","вересня","жовтня","листопада","грудня"];
+order = true;
+
+var button = document.querySelector('.planService');
+var sort = document.querySelector('.sort');
+
+button.addEventListener('click', function () {
+	let partner = document.querySelector('input.partner').value;
+	let date = document.querySelector('input.Date').value;
+	let hour = document.querySelector('input.Hour').value;
+	let type = document.querySelector('select.Type').value;
+
+	document.cookie = encodeURIComponent(date + ' ' + hour) + '=' + encodeURIComponent(partner + '|' + type)+'; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT';
+
+	document.querySelector('.addSchedule').classList.add('invisible');
+	document.body.style.overflow = 'initial';
+	addSchedule();
+});
+
+sort.addEventListener('click', function () {
+	order = !order;
+	addSchedule();
+});
+
+function addSchedule () {
+	var allCookie = document.cookie.split('; ').sort();
+	var hours = 0;
+	if(!order) {
+		allCookie = allCookie.reverse();
+	}
+	if(allCookie != ''){
+		document.querySelector('.service').innerHTML = '';
+		allCookie.forEach( function(element, index) {
+			var service = decodeURIComponent(element).split('=');
+			if(service != '') {
+				time = service[0].split(' ');
+				value = service[1].split('|');
+				date = time[0].split('-');
+				hour = time[1];
+				partner = value[0];
+				type = value[1];
+				let dateObj = new Date();
+			    let month = dateObj.getMonth()+1;
+			    let day = dateObj.getDate()
+			    let year = dateObj.getFullYear();
+			    // if(date[0] >= year && date[1] >= month && date[2] >= day) {
+			    if(date[0] == year && date[1] >= month && date[2] >= day || date[0] > year){
+			    	if(!document.getElementById(`${date[2]}|${date[1]}`)){
+			    		let dateBox = document.createElement('div');
+		                dateBox.setAttribute("id", `${date[2]}|${date[1]}`);
+		                dateBox.classList.add('dateBox');
+		                dateBox.innerHTML = `<h3>${date[2]} ${months[parseInt(date[1])]}</h3>`;
+		                document.querySelector('.service').appendChild(dateBox);
+			    	}
+
+			    	wrapper = document.createElement('div');
+			    	wrapper.classList.add('wrapper');
+			    	document.getElementById(`${date[2]}|${date[1]}`).appendChild(wrapper);
+
+			    	part = document.createElement('h4');
+			    	part.classList.add('partner');
+			    	part.innerHTML = partner;
+			    	wrapper.appendChild(part);
+
+			    	time = document.createElement('h5');
+			    	time.classList.add('time');
+			    	time.innerHTML = hour;
+			    	wrapper.appendChild(time);
+
+			    	close = document.createElement('div');
+			    	close.classList.add('close');
+			    	close.setAttribute("id", service[0]);
+			    	wrapper.appendChild(close);
+			    	close.addEventListener('click',function (e) {
+			    		cookie = encodeURIComponent(e.target.getAttribute('id'));
+			    		console.log(e.target);
+			    		document.cookie = cookie + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+			    		e.target.parentElement.classList.add('aniDelete');
+			    		setTimeout(function () {
+			    			e.target.parentElement.classList.add('invisible');
+			    		}, 300);
+			    	})
+
+			    }else if (date[0] == year && date[1] == month && parseInt(date[2]) < day) {
+			    	hours++;
+			    	console.log(1);
+			    }else {
+			    	console.log(year==date[0],month==date[1],month,date[1],day<date[2]);
+			    }
+			}
+		});	
+
+		document.querySelector('.hours').innerHTML = hours;
+	}
+}
---
+++ js/main.js
@@ -0,0 +1,80 @@
+var scheduledOpen = false;
+
+function changebleType () {
+	let input = document.querySelectorAll('.changable');
+
+    if(input) {
+        setTimeout(function () {
+            input[0].addEventListener('focus', function() {
+                input[0].type = 'date';
+            }, true);
+
+            input[0].addEventListener('blur', function() {
+                 input[0].type = 'text';
+            }, true);
+
+            input[1].addEventListener('focus', function() {
+                input[1].type = 'time';
+            }, true);
+
+            input[1].addEventListener('blur', function() {
+                 input[1].type = 'text';
+            }, true);  
+        }, 500);
+    }
+}
+
+
+
+window.addEventListener('scroll', function () {
+	var offtop = window.pageYoffset || document.documentElement.scrollTop;
+	var scheduled = document.querySelector('.scheduled');
+	if( offtop >= 253) {
+		scheduled.style.borderRadius = '0';
+		if (scheduledOpen == false) {
+			document.body.style.overflow = 'hidden';
+			scheduledOpen = true;
+			scroll(0,254);
+			setTimeout(function () {
+				document.body.style.overflow = 'initial';
+				scroll(0,254);
+			}, 150);
+		}
+	}else {
+		scheduledOpen = false;
+		scheduled.style.borderRadius = `${30-offtop*0.118110}px`
+	}
+});
+
+window.addEventListener('load', function () {
+	var newSchedule = document.querySelector('.newSchedule');
+	var close = document.querySelector('.close');
+	registerSW();
+	addSchedule();
+	newSchedule.addEventListener('click',function () {
+		document.querySelector('.addSchedule').classList.remove('invisible');
+		document.querySelector('.addSchedule .content').classList.add('aniSchedule');
+		document.body.style.overflow = 'hidden';
+
+		setTimeout(function () {
+			document.querySelector('.addSchedule .content').classList.remove('aniSchedule');
+		}, 10);
+	});
+
+	close.addEventListener('click',function () {
+		document.querySelector('.addSchedule').classList.add('invisible');
+		document.body.style.overflow = 'initial';
+	});
+
+	changebleType();
+});
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
